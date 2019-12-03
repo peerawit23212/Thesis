d70bb1e80b4fe8861691d8b384919b2615c41d79
Peerawit23212 <peerawitlimsombutanunt@gmail.com>
2019-12-03 22:34:37 +0700

    no message

---
+++ speech.js
@@ -0,0 +1,41 @@
+﻿var SpeechRecognition = SpeechRecognition || webkitSpeechRecognition
+var SpeechGrammarList = SpeechGrammarList || webkitSpeechGrammarList
+var SpeechRecognitionEvent = SpeechRecognitionEvent || webkitSpeechRecognitionEvent
+
+var recognition = new SpeechRecognition()
+recognition.lang = 'th-TH'
+recognition.interimResults =  false
+recognition.maxAlternatives = 1
+recognition.onspeechend = function (){
+	recognition.stop();
+}
+
+window.onload = function(){
+    document.getElementById("micbot").onclick = function(){
+   recognition.start();
+   var utterance = new SpeechSynthesisUtterance("Hello");
+utterance.lang = 'th-TH'
+utterance.pitch = 1
+utterance.rate = 1
+speechSynthesis.speak(utterance)
+}
+
+}
+
+recognition.onresult = function(event) {
+ for (var i = 0; i < event.results.length; i++){
+  var speechRecognitionResult = event.results[i]
+  for (var j = 0; j < speechRecognitionResult.length; j++){
+   var speechRecognitionAlternative = speechRecognitionResult[i]
+   console.log('Get word:', speechRecognitionAlternative.transcript)
+   console.log('Prob:', speechRecognitionAlternative.confidence)
+   if(speechRecognitionAlternative.transcript == "ไม่ค่อยดีเท่าไหร่"){
+	document.getElementById("textbot").innerHTML = "งั้นเรามาลองคุยกันดูนะเผื่อจะดีขึ้น";
+   }
+   else if(speechRecognitionAlternative.transcript == "ก็โอเคนะ"){
+	document.getElementById("textbot").innerHTML = "งั้นก็ดีแล้ว";
+   }
+  }
+ }
+}
+
